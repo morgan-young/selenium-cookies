c71bf8d094a2346e3742bd82acb6bc38e7d00478
morgan-young <morganyoung1212@gmail.com>
2022-11-02 22:21:30 +0000

    ready to go

---
+++ robot.py
@@ -0,0 +1,39 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException, TimeoutException   
+options = Options()
+#chrome_options.add_argument("--headless")
+options.add_argument("--incognito")
+options.add_argument("--window-size=1280x1696")
+
+# Global Variables
+chrome = webdriver.Chrome(("chromedriver"), options=options)
+
+
+def get_cookie_selenium():
+    chrome.get("https://www.forbes.com/billionaires")
+    chrome.find_element(By.XPATH, '//*[@id="truste-consent-button"]').click()
+    cookies = chrome.get_cookies()
+    cookie_for_requests = cookies[4]['value']
+    chrome.close()
+    return cookie_for_requests
+
+
+def req_with_cookie(cookie_for_requests):
+    cookies = dict(
+        Cookie=f'notice_preferences=2:; notice_gdpr_prefs=0,1,2::implied,eu; euconsent-v2={cookie_for_requests};')
+    r = requests.get("https://www.forbes.com/billionaires/page-data/index/page-data.json", cookies=cookies)
+    return r.json()
+
+
+if __name__ == '__main__':
+    data = req_with_cookie(get_cookie_selenium())
+    for row in range(10):
+        print(data['result']['pageContext']['tableData'][row]['employment']['name'])
